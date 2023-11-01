ed7541a2bf939843022e70f68b2df311a1b001ae
tamadam <60942087+tamadam@users.noreply.github.com>
2023-11-01 13:26:45 +0100

    Add error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,11 +2,12 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 interface IssueForm {
   title: string;
@@ -15,33 +16,48 @@ interface IssueForm {
 
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   const router = useRouter();
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root>
-        {/* ...register 4 property-t ad vissza, ezért kell a ...(spread) operator */}
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      {/* SimpleMDE nem támogat additional props-okat, emiatt a Controller kell
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <AiOutlineInfoCircle />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <TextField.Root>
+          {/* ...register 4 property-t ad vissza, ezért kell a ...(spread) operator */}
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        {/* SimpleMDE nem támogat additional props-okat, emiatt a Controller kell
       a field prop ugyanazt adja vissza mint a register*/}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <Button>Submit New Issue</Button>
-    </form>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
