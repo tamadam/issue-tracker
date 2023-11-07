7b28fe10c8dcc4726bce9444619ebd23701a7d04
tamadam <60942087+tamadam@users.noreply.github.com>
2023-11-07 17:08:29 +0100

    Disable SSR for SimpleMDE

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import React, { useState } from "react";
@@ -13,6 +13,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 /* interface IssueForm {
   title: string;
