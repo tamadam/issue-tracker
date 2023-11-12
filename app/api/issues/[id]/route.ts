7cc0df813ea09ebea82a784bd398e890fd446c3a
tamadam <60942087+tamadam@users.noreply.github.com>
2023-11-12 16:12:11 +0100

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { issueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -14,11 +14,23 @@ export async function PATCH (
     }
 
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
 
     if (!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 });
     
+    const { title, description, assignedToUserId } = body; // body.assignedToUserId
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({
+            where: {id: assignedToUserId},
+        });
+
+        if (!user) {
+            return NextResponse.json({error: "Invalid user."}, { status: 400 });
+        }
+    }
+
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id)}
     })
@@ -29,9 +41,14 @@ export async function PATCH (
     const updatedIssue = await prisma.issue.update({
         where: { id: issue.id },
         data: {
-            title: body.title,
-            description: body.description
-        }
+            //title: body.title,
+            //description: body.description,
+            // if any of these properties are not set or undefined, they won't be updated
+            title,
+            description,
+            assignedToUserId
+        },
+        
     })
 
     return NextResponse.json(updatedIssue);
